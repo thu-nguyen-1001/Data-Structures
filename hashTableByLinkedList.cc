d2bb79b57d2b77a51956b756dedea166b9394108
thu-nguyen-1001 <ntathu231001@gmail.com>
2022-01-10 23:12:17 -0800

    implement hash table

---
+++ hashTableByLinkedList.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+	int value;
+	Node* next;
+};
+
+class HashTable {
+private:
+	
+public:
+	HashTable() {
+
+	}
+	~HashTable() {
+
+	}
+	void insert(int val) {
+
+	}
+	void remove(int val) {
+
+	}
+	bool contains(int val) {
+
+	}
+
+}
+
+int main() {
+	return 0;
+}
